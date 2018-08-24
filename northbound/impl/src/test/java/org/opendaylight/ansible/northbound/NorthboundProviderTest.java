e089f25fd8f7d31219c096445456ed3c5a7b460e
Sam Hague <shague@gmail.com>
2018-08-23 21:49:37 -0400

    add runner test
    Signed-off-by: Sam Hague <shague@gmail.com>

--- northbound/impl/src/test/java/org/opendaylight/ansible/northbound/NorthboundProviderTest.java
+++ northbound/impl/src/test/java/org/opendaylight/ansible/northbound/NorthboundProviderTest.java
@@ -10,20 +10,33 @@ package org.opendaylight.ansible.northbound;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.opendaylight.ansible.mdsalutils.Datastore.CONFIGURATION;
+import static org.opendaylight.ansible.northbound.api.AnsibleTopology.ANSIBLE_TOPOLOGY_ID;
 import static org.opendaylight.ansible.northbound.api.AnsibleTopology.ANSIBLE_TOPOLOGY_PATH;
 
 import com.google.common.base.Optional;
 import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import org.junit.Before;
 import org.junit.Test;
+import org.opendaylight.ansible.mdsalutils.Datastore.Configuration;
+import org.opendaylight.ansible.mdsalutils.RetryingManagedNewTransactionRunner;
 import org.opendaylight.ansible.mdsalutils.SingleTransactionDataBroker;
+import org.opendaylight.ansible.mdsalutils.TypedReadTransaction;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
 import org.opendaylight.controller.md.sal.binding.test.ConstantSchemaAbstractDataBrokerTest;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,7 +52,7 @@ public class NorthboundProviderTest extends ConstantSchemaAbstractDataBrokerTest
 
     @Test
     public void testInitializeAnsibleTopology() {
-        ReadWriteTransaction transaction = dataBroker.newReadWriteTransaction();
+        ReadOnlyTransaction transaction = dataBroker.newReadOnlyTransaction();
         CheckedFuture<Optional<Topology>, ReadFailedException> ansibleTopology =
             transaction.read(LogicalDatastoreType.CONFIGURATION, ANSIBLE_TOPOLOGY_PATH);
         try {
@@ -59,5 +72,13 @@ public class NorthboundProviderTest extends ConstantSchemaAbstractDataBrokerTest
         } catch (ReadFailedException e) {
             LOG.error("NorthboundProvider error reading ansible topology", e);
         }
+
+        RetryingManagedNewTransactionRunner txRunner =
+            new RetryingManagedNewTransactionRunner(dataBroker, 3);
+        txRunner.callWithNewReadWriteTransactionAndSubmit(CONFIGURATION, tx -> {
+            Optional<Topology> ansibleTopology = tx.read(ANSIBLE_TOPOLOGY_PATH).get();
+            assertTrue(ansibleTopology.isPresent());
+            assertNotNull(ansibleTopology.get());
+        });
     }
 }
