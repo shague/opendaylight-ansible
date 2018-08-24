271a0b3a916bac187bb57f10f2a5676f316a4e9d
Sam Hague <shague@gmail.com>
2018-08-23 20:50:35 -0400

    add initial southbound module
    Signed-off-by: Sam Hague <shague@gmail.com>

---
+++ southbound/impl/src/main/java/org/opendaylight/ansible/southbound/SouthboundProvider.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.ansible.southbound;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class SouthboundProvider {
+    private static final Logger LOG = LoggerFactory.getLogger(SouthboundProvider.class);
+
+    @Inject
+    public SouthboundProvider(final DataBroker dataBroker) {
+        LOG.info("constructor");
+    }
+}
